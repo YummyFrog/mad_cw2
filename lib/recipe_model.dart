382bd0458e41d24ded23bc557f8f786c1016f846
Tommy Do <tommydo9876@gmail.com>
2025-02-20 01:29:02 -0500

    added recipe_model as a class. added homescreen, details scree, and main file

---
+++ lib/recipe_model.dart
@@ -0,0 +1,12 @@
+// recipe_model.dart
+class Recipe {
+  final String name;
+  final String ingredients;
+  final String instructions;
+
+  Recipe({
+    required this.name,
+    required this.ingredients,
+    required this.instructions,
+  });
+}
